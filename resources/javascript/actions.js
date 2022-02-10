24b5beda6d88bfd12daf5fedb28ed6ff4cd46f85
sdovgin <s1dovgin@gmail.com>
2022-02-10 15:22:00 -0500

    adding actions.js file

---
+++ resources/javascript/actions.js
@@ -0,0 +1,9 @@
+let about = document.getElementById('about');
+
+// Write your code here:
+function doAction(){
+    about.style.color = 'red';
+}
+
+//profile.addEventListener('click', doAction)
+about.onclick(doAction);
